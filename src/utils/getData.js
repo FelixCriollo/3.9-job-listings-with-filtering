ffc7ab0687f20c90e4de84d5797f0c6f3bbd201b
Felix Criollo <felixcriollovigo@gmail.com>
2022-05-27 10:33:51 -0500

    move data to public file3

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,5 +1,7 @@
+const data = new URL('/data/data.json', import.meta.url).href
+
 export async function getJobs() {
-    const response = await fetch("/data/data.json");
+    const response = await fetch(data);
     const listsJobs = await response.json();
 
     return listsJobs;
